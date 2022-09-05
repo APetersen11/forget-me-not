740a85a24b69f3ce7d4c54598eb1312ddce1bed7
Alyssa Petersen <aptrsn11@gmail.com>
2022-09-05 11:07:45 -0400

    add timeblacks via html

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Use moment to add the date to the top of the page
+
+// Compare actual time to time of time block to set past present and future classes
+
+// add a event listener to save the contents of the text area to local storage
+
+// load saved text area content from local storage
