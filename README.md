511ba4ecafae5d740a5fac72508e24738f3d59dc
Alyssa Petersen <aptrsn11@gmail.com>
2022-10-06 11:38:37 -0400

    add read me

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # forget-me-not
+
+This application is an hour by hour work day scheduler to help you remember all your appointments and obligations. Past appointments will appear in gray, the current time will appear in red, and upcoming times will appear in green. 
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Questions](#questions)
+
+## Installation
+
+No installation necessary.
+
+## Usage
+
+To use this application simply enter your obligations for that day and hit the save icon. Your text inputs will be remembered when you refresh the page. In addition, the current date will be displayed at the top of the page. 
+
+The repo for this application can be found here: [Forget Me Not Repo] (https://github.com/APetersen11/forget-me-not)
+
+And the deployed application can be found here: [Forget Me Not Application] (https://apetersen11.github.io/forget-me-not/)
+
+![screeenshot1](./assets/images/Screen%20Shot%202022-10-06%20at%2011.35.13%20AM.png)
+![screenshot2](./assets/images/Screen%20Shot%202022-10-06%20at%2011.35.24%20AM.png)
+
+## Contributing
+
+You may fork and contribute.
+
+## Questions
+
+If you have questions, please contact me on GitHub here: [APetersen11](https://github.com/APetersen11)
+or by email here: [aptrsn11@gmail.com](mailto:aptrsn11@gmail.com)
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,54 @@
 // Use moment to add the date to the top of the page
+const currentDate = moment().format("MM/DD/YY");
+document.getElementById("currentDay").textContent = currentDate;
 
 // Compare actual time to time of time block to set past present and future classes
 
+// get current hour
+function timeTracker() {
+  let currentHour = moment().hour();
+
+  // check time in time blocks
+  $(".time-block").each(function () {
+    let timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
+    console.log(timeBlockHour, currentHour);
+
+    if (timeBlockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (timeBlockHour === currentHour) {
+      $(this).addClass("present");
+      $(this).removeClass("past");
+    } else {
+      $(this).addClass("future");
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+    }
+  });
+}
+
+timeTracker();
+
 // add a event listener to save the contents of the text area to local storage
+$(".saveBtn").on("click", function () {
+  var key = $(this).parent().attr("id");
+
+  var value = $(this).siblings("textarea").val();
+
+  localStorage.setItem(key, value);
+});
 
 // load saved text area content from local storage
+//get items for each time block
+// $("#hour-9 textarea").val(localStorage.getItem("hour-9"));
+// $("#hour-10 textarea").val(localStorage.getItem("hour-10"));
+// $("#hour-11 textarea").val(localStorage.getItem("hour-11"));
+// $("#hour-12 textarea").val(localStorage.getItem("hour-12"));
+// $("#hour-13 textarea").val(localStorage.getItem("hour-13"));
+// $("#hour-14 textarea").val(localStorage.getItem("hour-14"));
+// $("#hour-15 textarea").val(localStorage.getItem("hour-15"));
+// $("#hour-16 textarea").val(localStorage.getItem("hour-16"));
+// $("#hour-17 textarea").val(localStorage.getItem("hour-17"));
+
+for(let i = 9; i < 18; i++){
+    $(`#hour-${i} textarea`).val(localStorage.getItem(`hour-${i}`));
+} 
